125785b8af458c2e06df7458ce6250ee832f4568
anbor <an@bor.com>
2023-03-15 18:18:15 +0200

    Test suiet wallet kit with bugs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
+import { WalletProvider } from '@suiet/wallet-kit'
 import type { AppProps } from 'next/app'
+import '@suiet/wallet-kit/style.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <WalletProvider>
+    <Component {...pageProps} />
+  </WalletProvider>
 }
